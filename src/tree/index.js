1139e6f6cc85032a10d9c77eb93762370d2f6276
J B <jdboykinsii@gmail.com>
2025-03-24 17:10:19 -0700

    Updating layout and style.

--- src/tree/index.js
+++ src/tree/index.js
@@ -3,14 +3,29 @@ import './index.css'
 
 export default function Tree() {
     return (
-      <div className="tree">
-        mammals<br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cheetah <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bear <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lion <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ape <br/>
-      </div>
+        <div className="tree">
+            <ul className="animalTree">
+                <li><a>mammals</a>
+                    <ul>
+                        <li><a>cheetah</a>
+                            <li>
+                                <li><a>bear</a></li>
+                                <li>
+                                    <ul>
+                                        <li><a>lion</a></li>
+                                        <li><a>dog</a>
+                                            <ul>
+                                                <li><a>elephant</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </li>
+                            <li><a>ape</a></li>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     )
 }
